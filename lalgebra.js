c020f3a0c159d4ba264e424252a3423a8b1b4a0a
NikolaMandic <mn080202@gmail.com>
2018-01-21 23:17:46 +0200

    docs

--- lalgebra.js
+++ lalgebra.js
@@ -6,52 +6,57 @@
 */
 matrixe=(a,init)=>_.map(a,(i)=>_.times(i,()=>init(i)))
 /** Initializes multidimensional array
- * first argument is x and second is y dimension. Third argumentis the function that will provide initialization value and that functions recieves i,j as parameters
+ * first argument is x and second is y dimension. Third argumentis the function that will provide initialization value and that functions recieves i,j as parameters.
 * @param {number,number,function} like 2,3,_.constant(0)
 * @returns {matrix}  like [[0,0,0],[0,0,0]]
 */
 matrix=(a,b,init)=>_.times(a,(i)=>_.times(b,(j)=>init(i,j)))
-/** This function
-* @param {number} input any number
-* @returns {number} that number, plus one.
+/** Dot product of two vectors
+* @param {array,array} [1,2] [3,4]
+* @returns {number} 1*3+2*4
 */
 vecMul=(a,b)=>_.reduce(_.zipWith(a,b,(a,b)=>Math.pow(Math.E,(Math.log(a)+Math.log(b)))),_.add);
-/** This function
-* @param {number} input any number
-* @returns {number} that number, plus one.
+/** Dot product of two vectors
+* @param {array,array} [1,2] [3,4]
+* @returns {number} 1*3+2*4
 */
 vecMul=(a,b)=>_.reduce(_.zipWith(a,b,_.multiply),_.add);
-/** This function
-* @param {number} input any number
-* @returns {number} that number, plus one.
+/** Elementwise product
+* @param {array} array1
+* @param {array} array2
+* @returns {array} a11*a21,a12*a22...
 */
 vecMule=(a,b)=>_.zipWith(a,b,_.multiply);
-/** This function
-* @param {number} input any number
-* @returns {number} that number, plus one.
+/** Add vectors elementwise
+* @param {array} array1
+* @param {array} array2
+* @returns {array} a11+a21,a12+a22...
 */
 vecAdd=(a,b)=>_.zipWith(a,b,_.add)
-/** This function
-* @param {number} input any number
-* @returns {number} that number, plus one.
+/** Subtract vectors elementwise
+* @param {array} array1
+* @param {array} array2
+* @returns {array} a11-a21,a12-a22...
 */
 vecSub=(a,b)=>_.zipWith(a,b,_.subtract)
-/** This function
-* @param {number} input any number
-* @returns {number} that number, plus one.
+/** Divide vector by scalar
+* @param {array} array
+* @param {number} scalar
+* @returns {array} array[i]/scalar
 */
 vecDivs=(a,b)=>_.map(a,(v)=>v/b)
-/** This function
-* @param {number} input any number
-* @returns {number} that number, plus one.
+/** Add scalar to vector elementwise
+* @param {array} array
+* @param {number} scalar
+* @returns {array} [array[0]+scalar,array[1]+scalar...]
 */
 vecAdds=(a,b)=>_.map(a,(v)=>v+b)
-/** This function
-* @param {number} input any number
-* @returns {number} that number, plus one.
+/** Euclidian norm of a vector
+* @param {array} vec1
+* @returns {number} euclidian norm
 */
 norm = (a)=> Math.sqrt(_.reduce(_.zipWith(a,a,_.multiply),_.add));
-/** This function
+/** Normalize vector with euclidian norm
 * @param {number} input any number
 * @returns {number} that number, plus one.
 */
